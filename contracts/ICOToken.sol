3cfc0dc0ed0ea0afb7d2a3f7c24b13ed5ea34efc
Derrick Asamoah <derrick.asamoah@amalitech.com>
2023-07-18 18:46:42 +0000

    ico token completed

--- contracts/ICOToken.sol
+++ contracts/ICOToken.sol
@@ -10,8 +10,24 @@ contract MyICO is ERC20 {
     uint256 public constant SALE_DURATION = 1 days;
 
     // insert contructor() function
+    constructor(uint256 _amount) ERC20("Juice", "ICO") {
+        owner = msg.sender;
+
+        _mint(owner, _amount);
+
+        startTime = block.timestamp;
+    }
 
     // insert ownerMint() function
+    function ownerMint(uint256 _amount) external {
+        require(owner == msg.sender, "Not the owner");
+        _mint(owner, _amount);
+    }
 
     // insert buyTokens() function
+    function buyTokens(uint256 _amount) external payable {
+        require(block.timestamp <= startTime + SALE_DURATION, "Sale has expired");
+        require(msg.value == _amount * 1/10, "Wrong amount of ETH sent");
+        _mint(msg.sender, _amount);
+    }
 }
